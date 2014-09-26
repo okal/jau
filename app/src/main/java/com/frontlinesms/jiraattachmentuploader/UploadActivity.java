d1d58bdbf9bd4dc89c10d6fb6704f034a489c35d
Okal Otieno <okal@justokal.com>
2014-09-26 11:58:35 +0300

    Open image picker when trigger text view is clicked

--- app/src/main/java/com/frontlinesms/jiraattachmentuploader/UploadActivity.java
+++ app/src/main/java/com/frontlinesms/jiraattachmentuploader/UploadActivity.java
@@ -1,17 +1,47 @@
 package com.frontlinesms.jiraattachmentuploader;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class UploadActivity extends ActionBarActivity {
+
+    private static int REQUEST_LOAD_IMAGE = 1;
+    private Button uploadButton;
+    private ImageView imagePreview;
+    private TextView clickToChooseView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+        uploadButton = (Button) findViewById(R.id.uploadButton);
+        imagePreview = (ImageView) findViewById(R.id.imagePreview);
+
+        clickToChooseView = (TextView) findViewById(R.id.uploadTrigger);
+        clickToChooseView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                    Intent.ACTION_PICK,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                );
+
+                startActivityForResult(intent, REQUEST_LOAD_IMAGE);
+            };
+        });
+
     }
 
 
@@ -32,5 +62,19 @@ public class UploadActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+
+            imagePreview.setImageURI(selectedImage);
+            uploadButton.setEnabled(true);
+            TextView clickToChooseView = (TextView) findViewById(R.id.uploadTrigger);
+            clickToChooseView.setVisibility(View.INVISIBLE);
+        }
     }
 }
