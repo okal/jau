9fd59c92e82c30e628ff9bd9421950772526217a
ivan <ivanoronee@gmail.com>
2014-09-26 12:14:48 +0300

    Introduced preferences activity

--- app/src/main/java/com/frontlinesms/jiraattachmentuploader/UploadActivity.java
+++ app/src/main/java/com/frontlinesms/jiraattachmentuploader/UploadActivity.java
@@ -1,12 +1,24 @@
 package com.frontlinesms.jiraattachmentuploader;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class UploadActivity extends ActionBarActivity {
+
+    private static final int RESULT_SETTINGS = 1;
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,19 +30,30 @@ public class UploadActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.upload, menu);
+        getMenuInflater().inflate(R.menu.settings, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
-            return true;
+            Intent i = new Intent(this, SettingsActivity.class);
+            startActivityForResult(i, RESULT_SETTINGS);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case RESULT_SETTINGS:
+                Context context = getApplicationContext();
+                prefs = PreferenceManager.getDefaultSharedPreferences(context);
+                //listeners for change in preferences
+                break;
+        }
     }
 }
